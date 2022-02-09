096c21c83bb0d87d1cf30833ca5daf11d537ebe0
Muktar16 <48786734+Muktar16@users.noreply.github.com>
2022-02-10 02:35:21 +0600

    Add files via upload

--- SkinDetection/test.py
+++ SkinDetection/test.py
@@ -10,29 +10,18 @@ for i in range (256):
             probability[i][j][k] = float(f.readline())
 f.close()
 
-# data to 3*3 matrix
-# i=0
-# j=0
-# k=0
 
-# for data in f:
-#     if(k==256):
-#         j=j+1
-#         if(j==256):
-#             i=i+1
-#     probability[i%256][j%256][k%256]=float(data)
-#     k=k+1
 
-testingImage = Image.open('0446.jpg')
+testingImage = Image.open('0234.jpg')
 testImLoader = testingImage.load()
 output_Image = testingImage.load()
 
-
+testingImage.show()
 for i in range (0, testingImage.size[0]):
     for j in range (0,testingImage.size[1]):
-        if probability[testImLoader[i,j][0]][testImLoader[i,j][1]][testImLoader[i,j][2]] >= 1.0:
+        if probability[testImLoader[i,j][0]][testImLoader[i,j][1]][testImLoader[i,j][2]] >= 1.3:
             output_Image[i,j] = testImLoader[i,j]
-            #output_Image[i,j] = (255,255,255)
+            #output_Image[i,j] = (0,0,0)
         else:
             output_Image[i,j] = (255,255,255)
 #testingImage.save("outputImage.bmp")
